f0e2374a1622e722488be36e36fdc17d0b552898
monotone2k <monotone2k@gmail.com>
2016-08-13 22:45:55 +0100

    Initial commit.

---
+++ controllers/employee.controller.js
@@ -0,0 +1,23 @@
+app.controller("employeeCtrl", function($scope, $http) {
+    $http.get('http://localhost/employee/').
+        success(function(data, status, headers, config) {
+            $scope.employees = data;
+        }).
+        error(function(data, status, headers, config) {
+            // log error
+        });
+	
+    $http.get('http://localhost/paymenthistory/').
+        success(function(data, status, headers, config) {
+            $scope.paymenthistory = data;
+        }).
+        error(function(data, status, headers, config) {
+            // log error
+        });
+	
+	$scope.saveEditEmployee = function(id) {
+		var data = $scope.fields;
+		var url = 'http://localhost/employee/' + id;
+		$http.post(url, data);
+	}
+});
---
+++ js/app.module.js
@@ -0,0 +1,40 @@
+var app = angular.module("payrollApp", ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+      
+    // For any unmatched url, send to /route1
+    $urlRouterProvider.otherwise("/")
+      
+    $stateProvider
+		.state('home', {
+			url: "/",
+			templateUrl: "partials/listemployees.html",
+			controller: function($scope) {}
+		})
+		.state('payemployee', {
+			url: "/payemployee/:id",
+			templateUrl: "partials/payemployee.html",
+			controller: function($scope, $stateParams) {}
+		})
+		.state('employeehistory', {
+			url: "/employeehistory/:id",
+			templateUrl: "partials/employeehistory.html",
+			controller: function($scope, $stateParams) {
+				$scope.currentEmployee = $stateParams.id;
+			}
+		})
+		.state('employeedetails', {
+			url: "/employeedetails/:id",
+			templateUrl: "partials/employeedetails.html",
+			controller: function($scope, $stateParams) {
+				$scope.currentEmployee = $stateParams.id;
+			}
+		})
+		.state('editemployeedetails', {
+			url: "/editemployeedetails/:id",
+			templateUrl: "partials/editemployeedetails.html",
+			controller: function($scope, $stateParams) {
+				$scope.currentEmployee = $stateParams.id;
+			}
+		})
+});
